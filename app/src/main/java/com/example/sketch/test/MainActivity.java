a2b36fbf1f0a35176456739da0b0b76a1e6fb80c
tuhin-cse <tuhinabdurrazzak@gmail.com>
2018-09-27 02:16:26 +0600

    Start

--- app/src/main/java/com/example/sketch/test/MainActivity.java
+++ app/src/main/java/com/example/sketch/test/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.sketch.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean issignedin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.login) {
+            setContentView(R.layout.activity_login);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
